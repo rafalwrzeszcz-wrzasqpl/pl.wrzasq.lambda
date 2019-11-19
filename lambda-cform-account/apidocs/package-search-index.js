2532816b7129b1e779aeff5e44c164c7bce518f0
Travis CI User <travis@example.org>
2019-11-19 23:24:35 +0000

    wagon-git commit

---
+++ lambda-cform-account/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"pl.wrzasq.lambda.cform.account"},{"l":"pl.wrzasq.lambda.cform.account.model"},{"l":"pl.wrzasq.lambda.cform.account.service"}]
---
+++ lambda-cform-account/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"pl.wrzasq.lambda.cform.account.service","l":"AccountManager"},{"p":"pl.wrzasq.lambda.cform.account.model","l":"AccountRequest"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"pl.wrzasq.lambda.cform.account","l":"Handler"}]
