5c5bcd69b05cdccf0aa24aba7a914958f26f0071
Travis CI User <travis@example.org>
2019-11-19 23:23:28 +0000

    wagon-git commit

---
+++ lambda-json/apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"pl.wrzasq.lambda.json","c":"ObjectMapperFactory","l":"createObjectMapper()"},{"p":"pl.wrzasq.lambda.json","c":"ObjectMapperFactory","l":"ObjectMapperFactory()","url":"%3Cinit%3E()"}]
